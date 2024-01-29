64aa49ece0c0aaaa641af9de4ca3acfb6ad786cf
kananbayramli <bayramli-kenan@mail.ru>
2024-01-29 15:39:40 +0400

    Post index

--- Data/Concrete/EfCore/SeedData.cs
+++ Data/Concrete/EfCore/SeedData.cs
@@ -47,6 +47,7 @@ namespace BlogApp.Data.Concrete.EfCore
                             IsActive = true,
                             PublishedOn = DateTime.Now.AddDays(-10),
                             Tags = context.Tags.Take(3).ToList(),
+                            Image = "1.jpg",
                             UserId = 1
                         },
                         new Post {
@@ -55,6 +56,7 @@ namespace BlogApp.Data.Concrete.EfCore
                             IsActive = true,
                             PublishedOn = DateTime.Now.AddDays(-5),
                             Tags = context.Tags.Take(2).ToList(),
+                            Image = "2.jpg",
                             UserId = 2
                         },  
                         new Post {
@@ -63,6 +65,7 @@ namespace BlogApp.Data.Concrete.EfCore
                             IsActive = true,
                             PublishedOn = DateTime.Now.AddDays(-20),
                             Tags = context.Tags.Take(2).ToList(),
+                            Image = "3.jpg",
                             UserId = 2
                         }
                     );
