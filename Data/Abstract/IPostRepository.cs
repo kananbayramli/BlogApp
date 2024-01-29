8ea791ea9819599d322a44b5df23e0d8cd94fa60
kananbayramli <bayramli-kenan@mail.ru>
2024-01-29 13:23:15 +0400

    Posts Repository

---
+++ Data/Abstract/IPostRepository.cs
@@ -0,0 +1,13 @@
+using BlogApp.Entity;
+
+namespace BlogApp.Data.Abstract
+{
+    public interface IPostRepository
+    {
+        //ienumerable butun postlari getirir lakin iqureable filterleyerek, bize lazim olan meselen 5 postu getirir
+        IQueryable<Post> Posts{ get; }
+        
+        void CreatePost(Post post);
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using BlogApp.Data.Abstract;
 using BlogApp.Data.Concrete.EfCore;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<BlogContext>(options =>{
     var config = builder.Configuration;
@@ -10,12 +13,14 @@ builder.Services.AddDbContext<BlogContext>(options =>{
 });
 
 
+//addscoped her requeste bir obyect yaradir. 
+builder.Services.AddScoped<IPostRepository, EfPostRepository>();
 
 var app = builder.Build();
 
 SeedData.TestDatalariDoldur(app);
 
 
-app.MapGet("/", () => "Hello World!");
+app.MapDefaultControllerRoute();
 
 app.Run();
